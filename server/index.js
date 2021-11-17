439c7e81dc356c802f4d6a2e1f8561c040822740
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-17 16:24:01 -0700

    added css style to homepage

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 });
 
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../main.js'))
+});
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
