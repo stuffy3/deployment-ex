bef48997266fd6daee3e832f61dd20d7bc46317d
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-18 13:48:56 -0700

    initialized rollbar

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express")
 const path = require('path')
-
+const cors = require("cors")
 const app = express()
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9ab40165a66a4a168d87aed576f8c53e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
@@ -13,6 +22,8 @@ app.get('/stylesheet.css', (req, res) => {
   app.get('/ship.jpg', (req, res) => {
     res.sendFile(path.join(__dirname, '../ship.jpg'))
   })
+
+app.use(rollbar.errorHandler());
 
 const port = process.env.PORT || 4005;
 
